f63b0b7aea937d6c695460c8773063d637d61e05
Abhishek <abhishekc838@gmail.com>
2021-08-13 05:47:40 +0530

    hare krishna good morning

--- Portfolio page/assets/js/main.js
+++ Portfolio page/assets/js/main.js
@@ -97,15 +97,16 @@ modalCloses.forEach((modalClose) =>{
 
 let swiper = new Swiper(".portfolio_container", {
         cssMode: true,
+        loop: true,
+
         navigation: {
           nextEl: ".swiper-button-next",
           prevEl: ".swiper-button-prev",
         },
         pagination: {
           el: ".swiper-pagination",
+          clickable: true,
         },
-        mousewheel: true,
-        keyboard: true,
 });
 
 /*==================== TESTIMONIAL ====================*/
